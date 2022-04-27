4066bdecc12486d53b57a3d3cc89a2a9496403d8
Mithilesh Rai <raimithilesh@Mithileshs-MacBook-Pro.local>
2022-04-27 15:03:41 +0100

    Add more stages in pipeline

--- lib/devops-demo-stack.ts
+++ lib/devops-demo-stack.ts
@@ -1,12 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipeline, CodePipelineSource, ManualApprovalStep, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { MyPipelineAppStage } from './stage';
 
 export class DevopsDemoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('raimithilesh/devops-demo', 'main'),  
@@ -15,5 +16,17 @@ export class DevopsDemoStack extends Stack {
                    'npx cdk synth']
       })
     });
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "713411715170", region: "us-east-1" }
+    }));
+
+
+    testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+      env: { account: "713411715170", region: "us-east-1" }
+    }));
   }
 }
