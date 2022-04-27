3815b48ef9ed369b1edf5fce21b88a533e23ff02
Mithilesh Rai <raimithilesh@Mithileshs-MacBook-Pro.local>
2022-04-27 13:23:25 +0100

    Create a AWS PIPELINE

--- lib/devops-demo-stack.ts
+++ lib/devops-demo-stack.ts
@@ -1,16 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DevopsDemoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DevopsDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('raimithilesh/devops-demo', 'main'),  
+        commands: ['npm ci', 
+                   'npm run build', 
+                   'npx cdk synth']
+      })
+    });
   }
 }
